f9c3d24e51b6bedaeed48b0dded41f0d144c4caf
tan <tanmaykm@gmail.com>
2013-05-04 03:44:25 +0530

    weakobjectiddict since weakkeydict is hash-consing

--- src/FastSubVector.jl
+++ src/FastSubVector.jl
@@ -1,11 +1,14 @@
 
-const _fastsv_gc_protect = WeakKeyDict{Vector, Vector}()
+#const _fastsv_gc_protect = WeakKeyDict{Vector, Vector}()
+const _fastsv_gc_protect = Dict{Uint, Vector}()
 
 function fast_sub_vec(arr::Vector, r::Range1{Int}) 
     if((r.start > 0) && (r.start+r.len-1 <= length(arr)))
         p = pointer(arr, r.start)
         newv = pointer_to_array(p, r.len, false)
-        _fastsv_gc_protect[newv] = arr
+        #_fastsv_gc_protect[newv] = arr
+        _fastsv_gc_protect[object_id(newv)] = arr
+        finalizer(newv, x->delete!(_fastsv_gc_protect, object_id(x)))
         return newv
     end
     throw(BoundsError())
